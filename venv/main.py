e164969c43e7730b82310e09c6df703a625301f0
qas <simmaksimus@gmail.com>
2022-12-19 20:17:36 +0300

    Added the skeleton of the "main"
    MINOR

--- venv/main.py
+++ venv/main.py
@@ -1,8 +1,24 @@
 import pygame
 
 
+fps = 60
+
+
 def main():
-    pass
+    global fps
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    running = True
+    clock = pygame.time.Clock()
+    while running:
+        screen.fill(pygame.Color('black'))
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                running = False
+
+        pygame.display.flip()
+        clock.tick(fps)
 
 
 if __name__ == "__main__":
