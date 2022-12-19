4b41e655ed03358d1518190b9180c9631c5a9d32
qas <simmaksimus@gmail.com>
2022-12-19 22:42:33 +0300

    Added a start_screen branch, designed start screen, made it work End of the 1st day
    HUGE UPDATE

--- venv/main.py
+++ venv/main.py
@@ -1,15 +1,50 @@
+import os
+import sys
 import pygame
 
 
-fps = 60
+FPS = 60
+WIDTH = 800
+HEIGHT = 600
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('game_data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen(screen, clock):
+
+    fon = pygame.transform.scale(load_image('fon.png'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.flip()
+        clock.tick(FPS)
 
 
 def main():
-    global fps
+    global FPS
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
-    running = True
     clock = pygame.time.Clock()
+    running = True
+    start_screen(screen, clock)
     while running:
         screen.fill(pygame.Color('black'))
         for event in pygame.event.get():
@@ -18,7 +53,7 @@ def main():
                 running = False
 
         pygame.display.flip()
-        clock.tick(fps)
+        clock.tick(FPS)
 
 
 if __name__ == "__main__":
